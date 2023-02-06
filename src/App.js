73623548f3e5e9c279c26516413d81eecc032cac
MattGDwyer <matthewgdwyer@gmail.com>
2023-02-06 16:49:08 +0700

    mvp complete

--- src/App.js
+++ src/App.js
@@ -5,27 +5,62 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
 
 
 
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  async function updateQuote() {
+    try {
+      const response = await fetch("https://api.quotable.io/random");
+      const { statusCode, statusMessage, ...data } = await response.json();
+      if (!response.ok) {
+        throw new Error( {statusCode} + {statusMessage} );
+      }
+      setData(data);
+    } catch (error) {
+      console.error(error);
+      setData({ content: "Oops... Something went wrong!" });
+    }
+  }
+
+  React.useEffect(() => {
+    updateQuote();
+  }, []);
+
+
+  if (!data) {
+    return null;
+  }
+  
+  const encodedContent = encodeURIComponent(data.content + " -" + data.author);
+  const twitterShareLink = `https://twitter.com/intent/tweet?text=${encodedContent}&source=`;
+
   return (
     <div className="App">
       <header className="App-header">
       <Container id="quote-box">
       <Row>
-        <Col id="text">The Quote Goes Here!</Col>
+        <Col id="text"><p>{data.content}</p></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col></Col>
-        <Col id="author">Author</Col>
+        <Col id="author"><cite>&mdash; {data.author}</cite></Col>
       </Row>
-      <Row>
-        <Col><a id="tweet-quote">Tweet This!</a></Col>
+      <Row style={{marginTop: 30}}>
+        <Col><a id="tweet-quote" target="_blank" href={twitterShareLink}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="black" 
+              className="bi bi-twitter" viewBox="0 0 16 16">
+              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 
+                6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+            </svg>
+          </a>
+        </Col>
         <Col id="new-quote">
-        <Button variant="primary">New Quote</Button>{' '}
+        <Button className="quoteButton" variant="primary" onClick={updateQuote} style={{backgroundColor: 'black', color: '#5F9EA0'}}>New Quote</Button>{' '}
 
         </Col>
       </Row>
@@ -37,15 +72,3 @@ function App() {
 
 export default App;
 
-// {/* <img src={logo} className="App-logo" alt="logo" />
-// <p>
-//   Edit <code>src/App.js</code> and save to reload.
-// </p>
-// <a
-//   className="App-link"
-//   href="https://reactjs.org"
-//   target="_blank"
-//   rel="noopener noreferrer"
-// >
-//   Learn React
-// </a> */}
